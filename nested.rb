66a2445f6970bfcfe80aea067e37502bf479c1ea
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-17 19:29:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     programmer_hash.merge({:yukihiro_matsumoto => {
+       :known_for => "Ruby",
+       :languages => ["LISP", "C"]})
 end
 
 def changing_alan
