0f7a47ab1cc5b5b535b7ef85cac06f4955f23b9f
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-17 19:24:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-	programmer_hash[:grace_hopper]
-
+		 programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,8 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-
-	programmer_hash[:alan_kay][:known_for]
+		 programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -82,11 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 programmer_hash[:yukihiro_matsumoto] = {
-			 :known_for => "Ruby",
-			 :languages => ["LISP", "C"]
-		 }
-		 programmer_hash
+
 end
 
 def changing_alan
@@ -106,9 +100,6 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-programmer_hash[:alan_kay][:known_for] = alans_new_info
-programmer_hash
 
 end
 
@@ -129,6 +120,5 @@ def adding_to_dennis
         }
      }
 
-		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-		 programmer_hash
+
 end
